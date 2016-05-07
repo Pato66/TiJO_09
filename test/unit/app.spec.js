a5c1b65f2e3ac47229faa47a7f9125e0e59939b3
Patryk Hudy <Pato66>
2016-05-07 10:02:25 +0200

    changes 2

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,15 +2,28 @@ describe('app', function () {
     'use strict';
 
     var answer = window.app;
-
-    /* WZOR it('should set username to john and password to 12345 when login is call', function () {
-        carPortal.login('john', '12345');
-        expect(carPortal.credentials).toEqual({username: 'john', password: '12345'});
-    }); */
-
+    
     describe('calculateArea', function () {
         it('should return area: 5 and message: Success', function () {
             expect(app.calculateArea(7,2,1,'Success', 'Error')).toEqual({area:5 ,message:"Success"});
+        });
+        it('should return area: 5 and message: Success', function () {
+            expect(app.calculateArea(2,5,1,'Success', 'Error')).toEqual({area:3 ,message:"Success"});
+        });
+        it('should return area: 5 and message: Success', function () {
+            expect(app.calculateArea(2,5,2,'Success', 'Error')).toEqual({area:1 ,message:"Success"});
+        });
+        it('should return area: 5 and message: Success', function () {
+            expect(app.calculateArea(3,5,2,'Success', 'Error')).toEqual({area:-1 ,message:"Error"});
+        });
+        it('should return area: 5 and message: Success', function () {
+            expect(app.calculateArea(6,5,2,'Success', 'Error')).toEqual({area:-4 ,message:"Error"});
+        });
+        it('should return area: 5 and message: Success', function () {
+            expect(app.calculateArea(10,5,2,'Success', 'Error')).toEqual({area:0 ,message:"Big null"});
+        });
+        it('should return area: 5 and message: Success', function () {
+            expect(app.calculateArea(3,6,2,'Success', 'Error')).toEqual({area:0 ,message:"Big null"});
         });
     });
 });
