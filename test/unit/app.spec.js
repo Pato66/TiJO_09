b7c2b3186e2805f471f926fe8d8a21c1ced02aab
Pato66 <patrykhudy@gmail.com>
2016-05-07 16:24:34 +0200

    changes 3

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -7,22 +7,22 @@ describe('app', function () {
         it('should return area: 5 and message: Success', function () {
             expect(app.calculateArea(7,2,1,'Success', 'Error')).toEqual({area:5 ,message:"Success"});
         });
-        it('should return area: 5 and message: Success', function () {
+        it('should return area: 3 and message: Success', function () {
             expect(app.calculateArea(2,5,1,'Success', 'Error')).toEqual({area:3 ,message:"Success"});
         });
-        it('should return area: 5 and message: Success', function () {
+        it('should return area: 1 and message: Success', function () {
             expect(app.calculateArea(2,5,2,'Success', 'Error')).toEqual({area:1 ,message:"Success"});
         });
-        it('should return area: 5 and message: Success', function () {
+        it('should return area: -1 and message: Error', function () {
             expect(app.calculateArea(3,5,2,'Success', 'Error')).toEqual({area:-1 ,message:"Error"});
         });
-        it('should return area: 5 and message: Success', function () {
+        it('should return area: -4 and message: Error', function () {
             expect(app.calculateArea(6,5,2,'Success', 'Error')).toEqual({area:-4 ,message:"Error"});
         });
-        it('should return area: 5 and message: Success', function () {
+        it('should return area: 0 and message: Big null', function () {
             expect(app.calculateArea(10,5,2,'Success', 'Error')).toEqual({area:0 ,message:"Big null"});
         });
-        it('should return area: 5 and message: Success', function () {
+        it('should return area: 0 and message: Big null', function () {
             expect(app.calculateArea(3,6,2,'Success', 'Error')).toEqual({area:0 ,message:"Big null"});
         });
     });
